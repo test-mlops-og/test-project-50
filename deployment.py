aa56063dfb24ef0cc17621076e809832988ec647
ken-yeung-og <ken.yeung@opsguru.io>
2025-03-30 04:49:31 +0000

    [DEPLOY]

--- deployment.py
+++ deployment.py
@@ -5,6 +5,7 @@ def install_packages(packages):
     """
     Installs the given list of packages using pip.
     """
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
     for package in packages:
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
